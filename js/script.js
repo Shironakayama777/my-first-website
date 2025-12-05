c64e9d0eae5115088ee574dadad0aae4460c64e1
Shironakayama777 <mayangsari488@gmail.com>
2025-12-05 20:23:35 +0700

    update code terbaru

--- js/script.js
+++ js/script.js
@@ -17,3 +17,29 @@ if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
   navbarNav.classList.remove("active");
 }
 })
+
+// cart section
+const cartDrawer = document.querySelector(".cart-drawer");
+const cartButton = document.querySelector("#shopping-cart");
+const closeCart = document.querySelector("#close-cart");
+
+// buka cart + scroll ke menu
+cartButton.onclick = (e) => {
+  e.preventDefault(); // biar tidak reload halaman
+
+  // scroll halus ke section menu
+  document.querySelector("#menu").scrollIntoView({
+    behavior: "smooth"
+  });
+
+  // setelah scroll sedikit → buka drawer
+  setTimeout(() => {
+    cartDrawer.classList.add("active");
+  }, 400);
+};
+
+// tombol tutup
+closeCart.onclick = () => {
+  cartDrawer.classList.remove("active");
+};
+
